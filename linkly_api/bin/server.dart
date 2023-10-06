60fb02fe69136326228d84f238b4916f9d0585ce
jvitorthomaz <jovitorthomaz2@gmail.com>
2023-10-06 11:55:39 -0300

    project configuration

--- linkly_api/bin/server.dart
+++ linkly_api/bin/server.dart
@@ -1,32 +1,56 @@
 import 'dart:io';
 
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
+import 'package:args/args.dart';
+import 'package:dart_application/application/config/aplication.config.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
 
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
-}
-
+const _hostname = '0.0.0.0';
 void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+  var parser = ArgParser()..addOption('port', abbr: 'p');
+  var result = parser.parse(args);
 
-  // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  // For Google Cloud Run, we respect the PORT environment variable
+  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
+  var port = int.tryParse(portStr);
 
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  if (port == null) {
+    stdout.writeln('Could not parse port value "$portStr" into a number.');
+    // 64: command line usage error
+    exitCode = 64;
+    return;
+  }
+
+  // final router = Router();
+  // router.get('/health', (shelf.Request request) => shelf.Response.ok(jsonEncode({'up': 'true'})));
+  final appConfig = ApplicationConfig();
+  await appConfig.loadConfigApplication();
+
+  // // final getIt = GetIt.I;
+  // // getIt.registerSingleton(appConfig);
+  
+  final handler = const shelf.Pipeline()
+      // .addMiddleware(CorsMiddlewares().handler)
+      // .addMiddleware(DefaultContentType('application/json;charset=utf-8').handler)
+      // .addMiddleware(SecurityMiddleware(getIt.get()).handler)
+      .addMiddleware(shelf.logRequests())
+      .addHandler(_echoRequest);
+
+  final server = await io.serve(handler, _hostname, port);
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+shelf.Response _echoRequest(shelf.Request request) =>
+  shelf.Response.ok('Request for "${request.url}"');
+  // // Use any available host or container IP (usually `0.0.0.0`).
+  // final ip = InternetAddress.anyIPv4;
+
+  // // Configure a pipeline that logs requests.
+  // final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  // // For running in containers, we respect the PORT environment variable.
+  // final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  // final server = await serve(handler, ip, port);
+  // print('Server listening on port ${server.port}');
+
---
+++ linkly_api/lib/application/config/aplication.config.dart
@@ -0,0 +1,16 @@
+
+import 'package:dotenv/dotenv.dart' show load, env;
+
+class ApplicationConfig {
+
+  Future<void> loadConfigApplication() async {
+    await _loadEnv();
+    final variavel = env['url_banco_de_dados'];
+    final flutterHome = env['FLUTTER_HOME'];
+    print(variavel);
+    print(flutterHome);
+
+    
+  }
+  Future<void> _loadEnv() async => load();
+}
